b06e605d669bfeedd64949b1e7be8621a1197970
Morshed Himel <icesiv@gmail.com>
2023-09-20 20:58:29 +0600

    init

--- src/items_grab.py
+++ src/items_grab.py
@@ -23,57 +23,30 @@ async def main():
         await page.wait_for_selector('div.carousel__component')
         # Now logged in
 
+        page.on("response", lambda response: check_response(response, page))
+
+        # links = (helpers.load_items('product_link',
+        #                             constant.EXCEL_LIST_FILE_PATH))
+
+        # for l in links:
+        #     print(l)
+
         item_url = "/clarks-us/en_US/USD/c/Leisa-Cacti-Q/p/26109039"
+
         full_url = constant.BASE_URL + item_url
-
+        print("item_url: " + full_url)
         await page.goto(full_url)
-        await page.route("**/*.json", lambda route: route.continue_())
-
-        json_responses = []
-
-        async def capture_json_response(response):
-            request_url = response.request.url
-            if 'json' in request_url:
-                json_responses.append(response)
-
-        # Intercept and capture the JSON request
-        page.on('response', capture_json_response)
-
-        # Wait for the specific JSON request to be captured (you can specify a timeout)
-        await asyncio.sleep(5)  # Wait for 5 seconds or adjust as needed
-
-        # Extract and parse the JSON data from the captured responses
-        for json_response in json_responses:
-            json_data = await json_response.json()
-            print(json_data)
-
- # ________________________________________________________________________________
-
-        # page.on("response", lambda response: check_response(response, page))
-
-        # # links = (helpers.load_items('product_link',
-        # #                             constant.EXCEL_LIST_FILE_PATH))
-
-        # # for l in links:
-        # #     print(l)
-
-        # item_url = "/clarks-us/en_US/USD/c/Leisa-Cacti-Q/p/26109039"
-
-        # load_page(page, item_url)
-
-
-# def load_page(page, url):
-#     full_url = constant.BASE_URL + url
-#     print("item_url: " + full_url)
-#     page.goto(full_url)
 
 # https://api.bazaarvoice.com/data/products.json?passkey=aepj6ujftw8qdkioxfsc7k28s&apiversion=5.5&displaycode=19339-en_us&filter=id%3Aeq%3A26109039&limit=1&callback=bv_1111_19884
 # https://us.clarksone.com/p/futureStocks?productCodes=261090393045,261090393050,261090393055,261090393060,261090393065,261090393070,261090393075,261090393085,261090393095,261090394025,261090394030,261090394035,261090394040,261090394045,261090394050,261090394055,261090394065,261090394075,261090394085,261090394095,261090395035,261090395040,261090395045,261090395050,261090395055,261090395060,261090395065,261090395070,261090395075,261090395085,261090395095,261090393045,261090393050,261090393055,261090393060,261090393065,261090393070,261090393075,261090393085,261090393095,261090394025,261090394030,261090394035,261090394040,261090394045,261090394050,261090394055,261090394065,261090394075,261090394085,261090394095,261090395035,261090395040,261090395045,261090395050,261090395055,261090395060,261090395065,261090395070,261090395075,261090395085,261090395095,
 
 
-# def check_response(response, page):
+async def check_response(response, page):
+    if "products.json" in response.url:
+        print(response.url)
+        print(response)
+        print("*" * 30)
 
-#     print(response.url)
     # if "page-fragment" in response.url:
     #     print("-" * 50)
     #     print("url: " , response)
